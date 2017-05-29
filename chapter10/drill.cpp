5bc0979d03dd785792c4cb2f381ff37b3acb35c5
likui911 <Likui_911@163.com>
2017-05-29 16:23:01 +0800

    drill

---
+++ chapter10/drill.cpp
@@ -0,0 +1,46 @@
+#include "drill.h"
+#include "../std_lib_facilities.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+int main()
+{
+    using namespace std;
+    cout << "Please input 7 pairs (x,y):" << endl;
+    vector<Pointer> original_points{};
+    for (int i = 0; i < 7; ++i)
+    {
+        char c;
+        int x, y;
+        cin >> c >> x >> c >> y >> c;
+        original_points.push_back(Pointer{x, y});
+    }
+
+    ofstream ofs{"./chapter10/mydata.txt"};
+    if (!ofs)
+        error("can't open file");
+    for (Pointer p : original_points)
+    {
+        ofs << "(" << p.x << "," << p.y << ")" << endl;
+    }
+    ofs.close();
+    ifstream ifs{"./chapter10/mydata.txt"};
+    vector<Pointer> provessed_points{};
+    if (!ifs)
+        error("can't open file");
+    while (ifs)
+    {
+        char c;
+        int x, y;
+        ifs >> c >> x >> c >> y >> c;
+        provessed_points.push_back(Pointer{x, y});
+    }
+    for (Pointer p : provessed_points)
+    {
+        cout << "(" << p.x << "," << p.y << ")" << endl;
+    }
+    ifs.close();
+    
+    return 0;
+}
---
+++ chapter10/drill.h
@@ -0,0 +1,12 @@
+/*
+Start a program to work with points, discussed in §10.4. Begin by defining the data type 
+Point that has two coordinate members x and y
+Using the code and discussion in §10.4, prompt the user to input seven (x,y) pairs. As the 
+data is entered, store it in a vector of Points called original_points
+*/
+
+struct Pointer
+{
+    int x;
+    int y;
+};
