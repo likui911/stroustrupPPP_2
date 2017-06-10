c5c638a542eadf45b1cf752f11ae0e3789eb861a
likui911 <Likui_911@163.com>
2017-06-10 11:51:46 +0800

    ex06

--- chapter17/ex06.cpp
+++ chapter17/ex06.cpp
@@ -3,3 +3,18 @@ This chapter does not say what happens when you run out of memory using new. Tha
 out what happens. You have two obvious alternatives: look for documentation, or write a program with an infinite loop that
 allocates but never deallocates. Try both. Approximately how much memory did you manage to allocate before failing?
 */
+
+void allocate()
+{
+    int* a=new int[1024];
+}
+
+int main()
+{
+    //memory cost 398.5Mb
+    for(int i=0;i<100000;++i)
+    {
+        allocate();
+    }
+    return 0;
+}
