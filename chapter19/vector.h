2222902e15d91290c42784104ea0f762fed4bdf1
likui911 <Likui_911@163.com>
2017-09-18 13:06:11 +0800

    vector

--- chapter19/vector.h
+++ chapter19/vector.h
@@ -1,46 +1,98 @@
 #include <memory>
 #include <iostream>
+
 using std::allocator;
 using std::initializer_list;
 
 template <typename T, typename A = allocator<T>>
 class vector
 {
-	A alloc;
-	T *elem;
-	size_t sz;
-	size_t space;
-
   public:
+	typedef T *iterator;
+	typedef const T *const_iterator;
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+	typedef unsigned int size_type;
+
 	vector() : sz{0}, space{0}, elem{nullptr} {};
 	~vector();
-	explicit vector(std::size_t n, const T &val = T());
+	explicit vector(size_type n, const T &val = T());
 	explicit vector(initializer_list<T> lst);
-	explicit vector(const vector &vec);
-	explicit vector(const vector &&vec);
+	vector(const vector &vec);
+	vector(vector &&vec);
 
 	vector &operator=(const vector &vec);
-	vector &operator=(const vector &&vec);
+	vector &operator=(vector &&vec);
 
-	size_t size() { return sz; }
-	void resize(size_t n, const T &val = T());
-	size_t capacity() { return space; }
-	void reserve(size_t n);
+	iterator begin();
+	const_iterator begin() const;
+	reverse_iterator rbegin();
+	const_reverse_iterator rbegin() const;
+	reverse_iterator rend();
+	const_reverse_iterator rend() const;
+	const_iterator cbegin() const;
+	const_iterator cend() const;
+	const_reverse_iterator crbegin() const;
+	const_reverse_iterator crend() const;
 
-	T &operator[](size_t i);
-	const T &operator[](size_t i) const;
+	size_type size()
+	{
+		return sz;
+	}
+	void resize(size_type n, const T &val = T());
+	size_type capacity()
+	{
+		return space;
+	}
+	bool empty() const
+	{
+		return sz == 0;
+	};
+	void reserve(size_type n);
+	void shrink_to_fit();
+
+	T &operator[](size_type i);
+	const T &operator[](size_type i) const;
+	T &at(size_type i);
+	const T &at(size_type i) const;
+	T &front()
+	{
+		return elem[0];
+	}
+	const T &front() const
+	{
+		return elem[0];
+	}
+	T &back()
+	{
+		return elem[sz];
+	}
+	const T &back() const
+	{
+		return elem[sz];
+	}
+	T *data()
+	{
+		return &elem[0];
+	}
+	const T *data() const
+	{
+		&elem[0];
+	}
 
 	void push_back(const T &val);
-	void pop_back(const T &val);
+	void pop_back();
 
-	void print()
-	{
-		for (int i = 0; i < sz; ++i)
-		{
-			std::cout << elem[i] << std::endl;
-		}
-	}
+	void swap(vector &x);
+	void clear();
+
+  private:
+	A alloc;
+	T *elem;
+	size_type sz;
+	size_type space;
 };
+
 
 template <typename T, typename A>
 vector<T, A>::vector(initializer_list<T> lst)
@@ -65,7 +117,7 @@ vector<T, A>::~vector()
 }
 
 template <typename T, typename A>
-vector<T, A>::vector(const vector &vec)
+vector<T, A>::vector(const vector<T, A> &vec)
 {
 	T *p = alloc.allocate(vec.sz);
 	for (int i = 0; i < vec.sz; ++i)
@@ -78,14 +130,14 @@ vector<T, A>::vector(const vector &vec)
 }
 
 template <typename T, typename A>
-vector<T, A>::vector(const vector &&vec) : sz{vec.sz}, space{vec.space}, elem{vec.elem}
+vector<T, A>::vector(vector<T, A> &&vec) : sz{vec.sz}, space{vec.space}, elem{vec.elem}
 {
 	vec.sz = 0;
 	vec.elem = nullptr;
 }
 
 template <typename T, typename A>
-vector<T, A>::vector(size_t n, const T &val) : sz{n}, space{sz}
+vector<T, A>::vector(size_type n, const T &val) : sz{n}, space{sz}
 {
 	elem = alloc.allocate(n);
 	for (int i = 0; i < sz; ++i)
@@ -95,7 +147,7 @@ vector<T, A>::vector(size_t n, const T &val) : sz{n}, space{sz}
 }
 
 template <typename T, typename A>
-vector<T, A> &vector<T, A>::operator=(const vector &vec)
+vector<T, A> &vector<T, A>::operator=(const vector<T, A> &vec)
 {
 	if (this == &vec)
 	{
@@ -119,7 +171,7 @@ vector<T, A> &vector<T, A>::operator=(const vector &vec)
 }
 
 template <typename T, typename A>
-vector<T, A> &vector<T, A>::operator=(const vector &&vec)
+vector<T, A> &vector<T, A>::operator=(vector<T, A> &&vec)
 {
 	for (int i = 0; i < sz; ++i)
 	{
@@ -134,16 +186,55 @@ vector<T, A> &vector<T, A>::operator=(const vector &&vec)
 	return *this;
 }
 
+// todo
+// iterator begin();
+// const_iterator begin() const;
+// reverse_iterator rbegin();
+// const_reverse_iterator rbegin() const;
+// reverse_iterator rend();
+// const_reverse_iterator rend() const;
+// const_iterator cbegin() const;
+// const_iterator cend() const;
+// const_reverse_iterator crbegin() const;
+// const_reverse_iterator crend() const;
+
 template <typename T, typename A>
-void vector<T, A>::resize(size_t n, const T &val)
+void vector<T, A>::resize(size_type n, const T &val)
 {
-	//如果n<sz,移出并销毁超出的部分
-	//如果sz<n<space,构造未初始化空间
-	//如果n>space,重新分配空间
+	if (n < sz)
+	{
+		for (int i = n; i < sz; ++i)
+		{
+			alloc.destroy(&elem[i]);
+		}
+		sz = n;
+	}
+	else if (n > sz && n < space)
+	{
+		for (int i = sz; i < n; ++i)
+		{
+			alloc.construct(&elem[i], val);
+		}
+		sz = n;
+	}
+	else
+	{
+		T *p = alloc.allocate(n);
+		for (int i = 0; i < sz; ++i)
+		{
+			alloc.construct(&p[i], elem[i]);
+		}
+		for (int i = sz; i < n; ++i)
+		{
+			alloc.construct(&p[i], val);
+		}
+		sz = n;
+		space = n;
+	}
 }
 
 template <typename T, typename A>
-void vector<T, A>::reserve(size_t n)
+void vector<T, A>::reserve(size_type n)
 {
 	if (n <= space)
 	{
@@ -157,7 +248,7 @@ void vector<T, A>::reserve(size_t n)
 	}
 	for (int i = 0; i < sz; ++i)
 	{
-		alloc.destroy(*elem[i]);
+		alloc.destroy(&elem[i]);
 	}
 	alloc.deallocate(elem, space);
 
@@ -166,7 +257,98 @@ void vector<T, A>::reserve(size_t n)
 }
 
 template <typename T, typename A>
-T &vector<T, A>::operator[](size_t n)
+void vector<T, A>::shrink_to_fit()
+{
+	T *p = alloc.allocate(sz);
+	for (int i = 0; i < sz; ++i)
+	{
+		alloc.construct(&p[i], elem[i]);
+	}
+
+	for (int i = 0; i < sz; ++i)
+	{
+		alloc.destroy(&elem[i]);
+	}
+	alloc.deallocate(elem, space);
+
+	elem = p;
+	space = sz;
+}
+
+template <typename T, typename A>
+T &vector<T, A>::operator[](size_type n)
 {
 	return elem[n];
+}
+
+template <typename T, typename A>
+const T &vector<T, A>::operator[](size_type n) const
+{
+	return elem[n];
+}
+template <typename T, typename A>
+T &vector<T, A>::at(size_type n)
+{
+	if (sz << n)
+		throw std::out_of_range("acess out of range exception");
+	return elem[n];
+}
+
+template <typename T, typename A>
+const T &vector<T, A>::at(size_type n) const
+{
+	if (sz << n)
+		throw std::out_of_range("acess out of range exception");
+	return elem[n];
+}
+
+template <typename T, typename A>
+void vector<T, A>::push_back(const T &val)
+{
+	if (sz == 0)
+	{
+		reserve(8);
+	}
+	if (sz == space)
+	{
+		reserve(2 * sz);
+	}
+	alloc.construct(&elem[sz], val);
+	sz++;
+}
+
+template <typename T, typename A>
+void vector<T, A>::pop_back()
+{
+	alloc.destroy(&elem[sz]);
+	sz--;
+}
+
+template <typename T, typename A>
+void vector<T, A>::swap(vector &vec)
+{
+	if (this != &vec)
+	{
+		size_type _size = sz;
+		size_type _space = space;
+		T *_elem = elem;
+
+		elem = vec.elem;
+		vec.elem = _elem;
+
+		sz = vec.sz;
+		vec.sz = _size;
+		space = vec.space;
+		vec.space = _space;
+	}
+}
+
+template <typename T, typename A>
+void vector<T, A>::clear()
+{
+	for (int i = 0; i < sz; ++i)
+	{
+		alloc.destroy(&elem[i]);
+	}
+	sz = 0;
 }
--- chapter19/vector_test.cpp
+++ chapter19/vector_test.cpp
@@ -1,13 +1,18 @@
 #include "vector.h"
+#include <iostream>
+using namespace std;
+
+
+vector<int> make_vec()
+{
+	vector<int> res{1,2,3,4,5,6,7,8,9,0};
+	//这里按理说应该调用vector(vector&&)移动构造
+	return res;
+}
 
 int main()
 {
-	vector<int> vec1;
-	vector<int> vec2(10);
-	vector<int> vec3{1,2,3,4,5,6,7,8,9,0};
-	vector<int> vec4(vec3);
-	vec2=vec4;
-	vec4=vec4;
-	vec2.print();
+	std::cout<<make_vec().size()<<std::endl;
+
 	return 0;
 }
